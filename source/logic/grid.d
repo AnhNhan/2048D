90b9b2a6d586ff19e678aaec9adb5a9e3ae6f987
Anh Nhan Nguyen <anhnhan@outlook.com>
2014-04-07 17:29:03 +0200

    Adding grid rotation function

--- source/logic/grid.d
+++ source/logic/grid.d
@@ -103,10 +103,10 @@ private:
         {
             tiles = tiles.rotate90CW;
         }
-        
+
         // Everything is rotated so we only have to move the tiles down
         // TODO: Actually move the tiles
-        
+
         // Rotate the tiles again
         static if (direction == MoveDirection.Up)
         {
@@ -179,18 +179,76 @@ unittest {
     writeln("Done.\n");
 }
 
-// Rotation function
+// Rotation function for grid
 
-import std.traits : isArray;
+import std.traits : isArray, isStaticArray;
+import std.range : ElementType;
 
-auto rotate90CW(G)(G grid, uint times = 1)
-    if (isArray!G)
+G rotate90CW(G)(G grid)
+    if (isArray!G && isArray!(ElementType!G))
+in
 {
-    //
+    assert(grid.length);
+    assert(grid[0].length);
+    foreach (r; grid)
+        assert(grid.length == r.length, "We only support quadratic grids.");
+}
+body
+{
+    G target;
+
+    auto len = grid.length;
+
+    static if (!isStaticArray!G)
+    {
+        target.length = len;
+        foreach (ref r; target)
+            r.length = len;
+    }
+
+    foreach (ii; 0..len)
+    {
+        foreach (jj; 0..len)
+        {
+            target[ii][jj] = grid[(len - 1) - jj][ii];
+        }
+    }
+
+    return target;
+}
+
+G rotate90CW(G)(G grid, uint times)
+    if (isArray!G && isArray!(ElementType!G))
+{
+    if (times == 1)
+    {
+        return grid.rotate90CW;
+    }
+    else
+    {
+        return grid.rotate90CW.rotate90CW(times - 1);
+    }
 }
 
 unittest {
+    writeln("logic.grid.rotate90CW.");
 
+    auto g1 = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+    ];
+
+    assert(g1.rotate90CW == [
+            [7, 4, 1],
+            [8, 5, 2],
+            [9, 6, 3],
+        ]);
+
+    assert(g1.rotate90CW.rotate90CW.rotate90CW.rotate90CW == g1);
+    assert(g1.rotate90CW.rotate90CW.rotate90CW.rotate90CW == g1.rotate90CW(4));
+
+    writeln("Done.\n");
 }
 
 // RNG helper functions
